2ca041d11a58cd0d747c304fd3aee3bc2de64dbc
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-08-05 18:35:02 -0700

    cleaned up data and start time (as character)

--- make_total_drawpattern.R
+++ make_total_drawpattern.R
@@ -54,7 +54,7 @@ DT_DHWProfiles.long[ , bedrooms := str_extract(DHWProfile, "[1-5]" ) ]
 # extract number of people 
 DT_DHWProfiles.long[ , people := str_extract_all(DHWDAYUSE, "[1-6]", simplify = TRUE)[1:length(DHWDAYUSE),2] ]
 
-# change day to yday
+# change name of day to yday
 setnames(DT_DHWProfiles.long, old = c("day"), new = c("yday"))
 
 #  load DT_DHWUSEs.Rdata
@@ -72,36 +72,21 @@ DT_total_drawpatterns[ , c("id", "s"):= NULL]
 # duration in seconds
 DT_total_drawpatterns[ , duration := duration * 60]
 
-# make datetime for day 0 in POSIXct format
-datetime <- ymd_hms("2009-01-01 00:00:00", tz= "America/Los_Angeles" ) 
+# make a data.table for 2009 of character dates & wday
+# where row number is yday 
+# make a year long list of days
+days_of_year <- seq(ymd("2009-01-01", tz="America/Los_Angeles"), 
+                    ymd("2009-12-31", tz="America/Los_Angeles"), 
+                    by="days")
 
-datetime <- ymd_hms(paste("2009-12-31", DT_total_drawpatterns$start), tz= "America/Los_Angeles" ) 
-  days(DT_total_drawpatterns$yday)
+DT_date2009 <- data.table(date=as.character(days_of_year, format="%F"),
+                          wday=wday(days_of_year, label = TRUE, abbr = TRUE))
 
 
-# look for February 29
-grep("2009-02-29", datetime)
-# integer(0)
-
-# look for times on time change days
-# started Sunday, March 8 at 2 a.m. ended on Sunday, November 1 at 2 a.m.
-sort(unique(grep("2009-03-08", datetime, value = TRUE)))
-# nothing between 02:00 and 03:00
-sort(unique(grep("2009-11-01", datetime, value = TRUE)))
-# only one time, 2009-11-01 01:19:12
-grep("2009-11-01 01:19:12", datetime, value = TRUE)
-# 2 entries
-
-# add the datetime
-DT_total_drawpatterns[, datetime := datetime]
-
-# look for timechange
-DT_total_drawpatterns[datetime==ymd_hms("2009-11-01 01:19:12", tz= "America/Los_Angeles")]
-# it's when 2E1 is on 2009-11-01, DHW4BR & DHW5BR
-
-# add month and day of month
-DT_total_drawpatterns[ , month:= month(datetime, label = TRUE, abbr = TRUE)]
-DT_total_drawpatterns[ , mday:= mday(datetime)]
+# add dates
+DT_total_drawpatterns[ , c("date","wday") := list( DT_date2009[yday,date],
+                                                   DT_date2009[yday,wday])
+                       ]
 
 # set order of columns
 names(DT_total_drawpatterns)
@@ -110,10 +95,9 @@ names(DT_total_drawpatterns)
 #   DHWDAYUSE     "3H1"
 #   bedrooms      1-5
 #   people        1-6
-#   datetime      ymd_hms as POSIXct in tz "America/Los_Angeles"
 #   yday          1-365
-#   month         "Jan" "Feb", etc.
-#   mday          1-31
+#   wday          Sun Mon Tue Wed Thu Fri Sat
+#   date          "yyyy-mm-dd" in tz "America/Los_Angeles"
 #   start         "hh:mm:ss" as character
 #   enduse        Bath	ClothesWasher	Dishwasher	Faucet	Shower
 #   duration	    seconds
@@ -121,10 +105,9 @@ names(DT_total_drawpatterns)
 #   hotFlow       GPM
 #   coldFlow      GPM 
 
-
 setcolorder(DT_total_drawpatterns,
             c("DHWProfile", "DHWDAYUSE", "bedrooms", "people", 
-              "datetime", "yday", "month", "mday", "start",
+              "yday", "wday", "date", "start",
               "enduse", "duration", "mixedFlow", "hotFlow", "coldFlow")
             )
 
@@ -139,17 +122,11 @@ write.csv(DT_total_drawpatterns,
           row.names = FALSE)
 
 # some brief data checks
-setorder(DT_total_drawpatterns, DHWProfile, DHWDAYUSE, datetime)
+setorder(DT_total_drawpatterns, DHWProfile, DHWDAYUSE, date, start)
 
-DT_total_drawpatterns[ , list(first = min(datetime),
-                              last  = max(datetime)),
-                       by=DHWProfile]
-#    DHWProfile               first                last
-# 1:     DHW1BR 2009-01-01 05:55:48 2009-12-31 22:39:00
-# 2:     DHW2BR 2009-01-01 05:55:48 2009-12-31 23:54:00
-# 3:     DHW3BR                <NA>                <NA>
-# 4:     DHW4BR 2009-01-01 07:38:24 2009-12-31 23:51:36
-# 5:     DHW5BR 2009-01-01 04:00:36 2009-12-31 22:39:00
-# still something wrong
+DT_total_drawpatterns[ , list(first = min(date),
+                              last  = max(date)),
+                       by=c('DHWProfile', 'enduse')]
+# seems OK
 
-DT_total_drawpatterns[ is.na(datetime),]
+
