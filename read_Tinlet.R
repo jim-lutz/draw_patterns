ffb3b7fc7a903e614eb609dc2c492a9cb1e6e25a
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-08-05 17:08:06 -0700

    plot of Tinlet by dayOfYear for all ctz

--- read_Tinlet.R
+++ read_Tinlet.R
@@ -86,41 +86,17 @@ anyDuplicated(DT_daily[,list(dayOfYear)])
 # [1] 69 found something
 
 DT_daily[duplicated(DT_daily[,list(dayOfYear)])]
+# 9283 
 
-DT_daily[dayOfYear==68,]
-#    dayOfYear   ctz  Tinlet
-# 1:        68 CTZ01 47.7885
-# 2:        68 CTZ01 47.8085
+DT_daily[dayOfYear==68,][order(ctz)]
+# 2 temperatures on day 68 in all 16 ctz
 
-# did some days have more than one temp?
-DT_Tinlet[ , list(n    = length(Tinlet),
-                  Tmin = min(Tinlet),
-                  Tmax = max(Tinlet),
-                  Tmode= mode(Tinlet)), by=dayOfYear][Tmin!=Tmax,]
-#    dayOfYear  n    Tmin    Tmax   Tmode
-# 1:        68 24 47.7885 47.8085 numeric
-# 2:        69 24 47.8085 47.8336 numeric
-# 3:        70 24 47.8336 47.8909 numeric
-# are the temps calculated without respect to time changes?
 
-DT_Tinlet[dayOfYear %in% 66:68,]
-#     dayOfYear month dayOfMonth hour DOWH  Tinlet   ctz
-# 23:        66     3          7   23    7 47.7577 CTZ01
-# 24:        66     3          7   24    7 47.7577 CTZ01
-# 25:        67     3          8    1    1 47.7885 CTZ01
-# 26:        67     3          8    2    1 47.7885 CTZ01
-# 27:        67     3          8    4    1 47.7885 CTZ01
-# 
-# 45:        67     3          8   22    1 47.7885 CTZ01
-# 46:        67     3          8   23    1 47.7885 CTZ01
-# 47:        67     3          8   24    1 47.7885 CTZ01
-# 48:        68     3          9    1    2 47.7885 CTZ01
-# 49:        68     3          9    2    2 47.8085 CTZ01
-# 50:        68     3          9    3    2 47.8085 CTZ01
-# 51:        68     3          9    4    2 47.8085 CTZ01
+DT_Tinlet[dayOfYear %in% 66:68 & ctz=='CTZ01',]
+# March 8 has no hour 03.
 # looks like Tinlet calculation doesn't follow time changes
 
-DT_Tinlet[dayOfYear%in% 65:70 , .N, by=c('dayOfYear','Tinlet')]
+DT_Tinlet[dayOfYear%in% 65:70 & ctz=='CTZ01', .N, by=c('dayOfYear','Tinlet')]
 #    dayOfYear  Tinlet  N
 # 1:        65 47.7191 24
 # 2:        66 47.7577 24
@@ -133,7 +109,7 @@ DT_Tinlet[dayOfYear%in% 65:70 , .N, by=c('dayOfYear','Tinlet')]
 # 9:        70 47.8909 23
 
 # this keeps the 365 dayOfYear with the most common Tinlet
-DT_Tinlet[, .N, by=c('dayOfYear','Tinlet')][order(-N)][361:370]
+DT_Tinlet[ ctz=='CTZ01', .N, by=c('dayOfYear','Tinlet')][order(-N)][361:370]
  #     dayOfYear  Tinlet  N
  #  1:       300 52.5301 23
  #  2:       301 52.5375 23
@@ -146,13 +122,31 @@ DT_Tinlet[, .N, by=c('dayOfYear','Tinlet')][order(-N)][361:370]
  #  9:        71 47.8909  1
  # 10:        72 47.9505  1
 
+# number of days by number of hours per day
+# for CTZ01
+DT_Tinlet[, .N, by=c('ctz','dayOfYear','Tinlet')][ctz=='CTZ01',  list(ndays = length(Tinlet)), by=N]
+#     N ndays
+# 1: 24   127
+# 2: 23   238
+# 3:  1   238
+
 # keep the 365 days with the most Tinlets
 DT_daily <-
-DT_Tinlet[, .N, by=c('dayOfYear','Tinlet')][order(-N)][1:365][order(dayOfYear)]
+  DT_Tinlet[, .N, by=c('ctz','dayOfYear','Tinlet')][order(-N)][1:(365*16)][order(ctz,dayOfYear)]
+# 5840, = 365*16
 
 # take a look, 
 ggplot(data = DT_daily) +
-  geom_point(aes(dayOfYear, Tinlet))
+  geom_point(aes(dayOfYear, Tinlet, color=ctz)) +
+  ggtitle("Inlet Water Temperatures") +
+  theme(plot.title = element_text(hjust = 0.5) ) +
+  labs(caption="from CBECC-Res19") +
+  xlab("Day of Year") +
+  ylab("deg F") + 
+  labs(color="climate zone")
 # still not as smooth as I'd expect for Tinlet
 # being overly influenced by air temps?
+
+# save the plot
+ggsave(filename = paste0(wd_charts,"Tinlet_by_ctz.png"))
 
