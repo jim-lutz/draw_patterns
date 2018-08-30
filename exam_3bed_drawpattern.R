cc3c3882e9c4e4b17cb080049cbc12e1f690a511
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-08-29 21:25:46 -0700

    exploring the range of draw patterns used in 3 bedroom annual profile

--- exam_3bed_drawpattern.R
+++ exam_3bed_drawpattern.R
@@ -42,8 +42,56 @@ ggplot(data = DT_3bed_people) +
 ggsave(filename = paste0("number_of_occupants.png"), path=wd_charts,
        width = 10.5, height = 8 )
 
+# the 48 DHWDAYUSE number of people, number of days
+DT_3bed_DHWDAYUSE <-
+  DT_3bed_daily[ , list(ndays   = length(yday),
+                        npeople = unique(people),
+                        totvol  = unique(totvol),
+                        ndraw   = unique(ndraw)),
+                 by=c('DHWDAYUSE')][order(DHWDAYUSE)]
+
+# find the 25th and 75th percentiles
+# of ndraw
+q.ndraw <- quantile(DT_3bed_daily$ndraw, probs = c(0.125, 0.875))
+# 25% 75% 
+#  38  79 
+# [1] 38 79
+str(q.ndraw)
+q.ndraw[['25%']]
+q.ndraw[[1]]
+
+# of totvol
+q.totvol <- quantile(DT_3bed_daily$totvol, probs = c(0.125, 0.875))
+#      25%      75% 
+# 35.98394 90.09349 
+
+# line segments
+line.segments <- data.frame(
+  x = unname(c(q.totvol, rev(q.totvol), q.totvol[[1]])),
+  y = unname(c(q.ndraw[[1]], q.ndraw[[1]], q.ndraw[[2]], q.ndraw[[2]], q.ndraw[[1]]))
+  )
+
+# scatter plot of volume vs number of draws per DHWDAYUSE
+ggplot(data=DT_3bed_DHWDAYUSE) +
+  geom_point(aes(x=totvol, y= ndraw, size=ndays, color=npeople), shape=1) +
+  geom_path(data=line.segments, aes(x=x, y=y)) +
+  ggtitle("Daily Draw Patterns", subtitle = "DHWDAYUSE") +
+  # center and format titles
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5,
+                                     size  = rel(0.75)))
 
 
+
+
+  
+  geom_jitter(aes(x=totvol, y= ndraw, size=people), 
+              width = 5, height = 5, alpha = 0.2 )  +
+  theme(plot.title = element_text(hjust = 0.5)) + # to center the title
+  scale_x_continuous(name = "total mixed water drawn (gallons/day)") +
+  scale_y_continuous(name = "total number of draws per day") +
+  labs(caption="from CBECC-Res19") #+ 
+================================
   
 
 
