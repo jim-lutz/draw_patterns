4065284cda61b91991bb22d05a66e8dc4539a740
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-09-02 22:41:08 -0700

    made a draw schedule, locations not yet hand identified, found some strange draws in the CBECC-Res draw pattern

--- make_drawpattern_schedule.R
+++ make_drawpattern_schedule.R
@@ -32,7 +32,7 @@ DT_selected[,list(ndate=length(unique(date))), by=c('DHWDAYUSE')][
 # 4:       3D2    11
 # 5:       4D5     4
 
-# fine the first date for each DHWDAYUSE in selected.DHWDAYUSE
+# find the first date for each DHWDAYUSE in selected.DHWDAYUSE
 DT_day1_DHWDAYUSE <-
   DT_selected[, list(date=date[1]), by=DHWDAYUSE]
 #    DHWDAYUSE       date
@@ -42,7 +42,7 @@ DT_day1_DHWDAYUSE <-
 # 4:       3D2 2009-04-09
 # 5:       4D5 2009-06-08
 
-# only those DHWDAYUSE & date
+# only those DHWDAYUSEs & dates
 DT_day1_selected <-
   merge(DT_selected, DT_day1_DHWDAYUSE,by=c('DHWDAYUSE','date'))
 
@@ -65,16 +65,53 @@ DT_day1_selected[,list(ndraws = length(start),
 # 5:       4D5     31 82.98847
 # yup, it's OK
 
+# sanity check on draws
+qplot(x=DT_day1_selected$duration, y=DT_day1_selected$mixedFlow,
+      xlab = "duration (secs)", ylab = "total flow rate (GPM)")
+# some pretty small flow ..
+
+qplot(x=DT_day1_selected$duration, y=DT_day1_selected$mixedFlow,
+      xlab = "duration (secs)", ylab = "total flow rate (GPM)",
+      ylim=c(0,1)) 
+
+qplot(x=DT_day1_selected$duration, y=DT_day1_selected$mixedFlow,
+      xlab = "duration (secs)", ylab = "total flow rate (GPM)",
+      ylim=c(0,0.1)) 
+
+# check on the outliers
+DT_day1_selected[ mixedFlow<0.1, list(DHWDAYUSE, date, start,
+                                      enduse, duration, mixedFlow)]
+#    DHWDAYUSE       date    start        enduse duration mixedFlow
+# 1:       2E2 2009-01-03 05:08:24        Faucet   100.02     0.047
+# 2:       2E2 2009-01-03 10:46:12        Faucet    79.98     0.064
+# 3:       2E2 2009-01-03 14:01:48        Faucet   109.98     0.031
+# 4:       2E2 2009-01-03 14:52:48        Faucet    79.98     0.038
+# 5:       2E2 2009-01-03 18:00:36        Faucet   150.00     0.034
+# 6:       4D5 2009-06-08 08:32:24 ClothesWasher   370.02     0.022
+# 7:       4D5 2009-06-08 08:38:24 ClothesWasher   360.00     0.025
+
+
+
+
 # build data.table first then worry about formatting and putting into Excel
 # https://cran.r-project.org/web/packages/xlsx/xlsx.pdf
 
-# build data columns to match
+# --- --- --- --- --- --- --- --- --- --- 
+# build data columns to match model spreadsheet
+# --- --- --- --- --- --- --- --- --- --- 
 # /home/jiml/HotWaterResearch/projects/How Low/pipe sizing/Model output example.xlsx
-# only 'Event_Type' == 'Use'
 
+
+# --- --- --- --- --- --- --- --- --- --- 
+# START TIME
+# --- --- --- --- --- --- --- --- --- --- 
 # add Start Time, date & start in a d/m/yyyy h:m:s format
 DT_day1_selected[ , Start_Time := paste(strftime(ymd(date),"%m/%d/%Y"),start)]
 
+
+# --- --- --- --- --- --- --- --- --- --- 
+# END USE
+# --- --- --- --- --- --- --- --- --- --- 
 # add Fixture ID, B2_SK1,B3_SK, K_SK, LN_WA, MB_SH, MB_SK1, MB_SK2, etc
 # for now just enter 2 letter enduse code as place holder
 DT_enduses <- 
@@ -88,6 +125,10 @@ DT_day1_selected <-
 # for now enter 'Use'
 DT_day1_selected[ , Event_Type := 'Use']
 
+
+# --- --- --- --- --- --- --- --- --- --- 
+# WAIT FOR HOT WATER
+# --- --- --- --- --- --- --- --- --- --- 
 # add Wait for Hot Water? 
 # Yes, (for shower, bath, & long (>= 1 min) faucet), No (everything else)
 DT_day1_selected[ , Wait_for_Hot_Water := 'No']
@@ -108,6 +149,9 @@ DT_day1_selected[,list(n = length(start)),
 # 6:        Shower                Yes   9
 # looks OK
 
+# --- --- --- --- --- --- --- --- --- --- 
+# BEHAVIOR WAIT
+# --- --- --- --- --- --- --- --- --- --- 
 # add Include Behavior Wait? Yes for shower, No for everything else.
 DT_day1_selected[ , Include_Behavior_Wait := 'No']
 DT_day1_selected[ enduse %in% c("Shower"), 
@@ -163,6 +207,10 @@ DT_day1_selected[,list(n = length(start)),
 # looks OK
 # set 0 to blank when exporting to Excel
 
+
+# --- --- --- --- --- --- --- --- --- --- 
+# DURATION
+# --- --- --- --- --- --- --- --- --- --- 
 # add Use time, duration (sec)
 # this is the total time of the draw, 
 # faucets > 60, subtract 60 for assumed clearing draw in CBECC-Res
@@ -188,105 +236,113 @@ DT_day1_selected[, list( n=length(start),
 DT_day1_selected[enduse=="Bath",]
 
 # look at all the baths
-DT_total_drawpatterns[enduse=='Bath', list(npeople   = unique(people),
-                                           duration  = unique(duration),
-                                           mixedFlow = unique(mixedFlow)),
+DT_day1_selected[enduse=='Bath', 
+                      list(npeople   = unique(people),
+                           duration  = unique(duration),
+                           mixedFlow = unique(mixedFlow)),
                       by=DHWDAYUSE][ , totvol := mixedFlow * duration / 60][
                         order(DHWDAYUSE)]
 # huh, totvol per bath seems low.
-#     DHWDAYUSE npeople duration mixedFlow    totvol
-#  1:       2D1       2   150.00     3.584  8.960000
-#  2:       2E2       2    79.98     2.820  3.759060
-#  3:       2E2       2   120.00     2.969  5.938000
-#  4:       3D1       3   169.98     3.457  9.793681
-#  5:       3D5       3   160.02     4.174 11.132058
-#  6:       3E2       3   160.02     4.787 12.766929
-#  7:       4D1       4   109.98     7.342 13.457886
-#  8:       4D3       4   139.98     3.790  8.842070
-#  9:       4E2       4   130.02     3.534  7.658178
-# 10:       4E2       4    49.98     3.886  3.237038
-# 11:       5D2       5   319.98     2.308 12.308564
-# 12:       5E1       5   180.00     4.596 13.788000
-# 13:       6D5       6   270.00     5.260 23.670000
+#    DHWDAYUSE npeople duration mixedFlow   totvol
+# 1:       2E2       2    79.98     2.820 3.759060
+# 2:       2E2       2   120.00     2.969 5.938000
+# 3:       3D1       3   169.98     3.457 9.793681
 # see http://www.allianceforwaterefficiency.org/Residential_Shower_Introduction.aspx
 # Showers vs Baths
 # for almost all of these the tub is less than 1/2 full
 # most ~ 1/4 full
 
+
+# --- --- --- --- --- --- --- --- --- --- 
+# --- FLOW RATES ---
+# --- --- --- --- --- --- --- --- --- --- 
 # add, Flow rate - waiting (GPM)
-#   faucet is 1.8, 
+#   lavatory faucet is 1.2, 
+#   kitchen faucet is 1.8 / 2.2
+#   shower is 1.8
 #   bath is 4.5, 
-#   master bath tubspout is 6
-# blank (0)  if 'Include Behavior Wait?' is No  (sec)(sec)	
+# blank (0)  if 'Include Behavior Wait?' is No  (sec)
 DT_day1_selected[, Flow_rate_waiting := 0]
 
 # list to flag rows by 'Wait for Hot Water' == Yes 
 # Wait_for_Hot_Water <- grepl("Yes", DT_day1_selected$'Wait for Hot Water')
 
 DT_day1_selected[ Wait_for_Hot_Water=='Yes' & enduse == 'Faucet',  
-                      Flow_rate_waiting := 1.8]
+                      Flow_rate_waiting := 1.2]
 DT_day1_selected[ Wait_for_Hot_Water=='Yes' & enduse == 'Shower',  
-                      Flow_rate_waiting := 4.0]
+                      Flow_rate_waiting := 1.8]
 DT_day1_selected[ Wait_for_Hot_Water=='Yes' & enduse == 'Bath',  
-                      Flow_rate_waiting := 6.0]
+                      Flow_rate_waiting := 4.5]
 
 # check that it worked
 DT_day1_selected[ Wait_for_Hot_Water=='Yes', 
                       list(enduse, Wait_for_Hot_Water, Flow_rate_waiting) ]
 
- add Flow rate - use (GPM)
+# add Flow rate - use (GPM)
 DT_day1_selected[, Flow_rate_use := mixedFlow]
 
-# check if Flow rate - use (GPM)
+# check if Flow rate - use (GPM) is above standard after location assigned
 #   bathroom faucet max is 1.2, 
 #   kitchen faucet max is 1.8
-DT_day1_selected[enduse=='Faucet' & Flow_rate_use > 1.2]
 
+# --- --- --- --- --- --- --- --- --- --- 
+# TEMPERATURE
+# --- --- --- --- --- --- --- --- --- --- 
 # add temperatures
 # Hot Water Temp, 125 (F)
 # Threshold Temp, 105 (F)	
 # Ambient Temp, 70 (F)
-DT_day1_selected[, c('Hot_Water_Temp', 'Threshold_Temp', 'Ambient_Temp') :=
-                       list(125, 105, 70)]
+DT_day1_selected[, c('Hot_Water_Temp', 'Threshold_Temp', 
+                     'Ambient_Temp', 'Cold_Water_Temp') :=
+                       list(125, 105, 70, 70)]
 
 names(DT_day1_selected)
-#  [1] "DHWProfile"            "DHWDAYUSE"             "bedrooms"             
-#  [4] "people"                "yday"                  "wday"                 
-#  [7] "date"                  "start"                 "enduse"               
+#  [1] "enduse"                "DHWDAYUSE"             "date"                 
+#  [4] "DHWProfile"            "bedrooms"              "people"               
+#  [7] "yday"                  "wday"                  "start"                
 # [10] "duration"              "mixedFlow"             "hotFlow"              
 # [13] "coldFlow"              "Start_Time"            "Fixture_ID"           
 # [16] "Event_Type"            "Wait_for_Hot_Water"    "Include_Behavior_Wait"
 # [19] "Behavior_Wait_Trigger" "Behavior_wait"         "Use_time"             
 # [22] "Flow_rate_waiting"     "Flow_rate_use"         "Hot_Water_Temp"       
-# [25] "Threshold_Temp"        "Ambient_Temp"         
+# [25] "Threshold_Temp"        "Ambient_Temp"          "Cold_Water_Temp"         
+
+# sort by DHWDAYUSE and Start_Time
+setkeyv(DT_day1_selected, cols = c('DHWDAYUSE','Start_Time'))
 
 # make a data.table for the Excel schedule
-DT_1day_schedule <- 
+DT_draw_schedule <- 
 DT_day1_selected[ , list(enduse,
-                             Event_Index = 2*.I-1,
-                             Start_Time,
-                             Fixture_ID,
-                             Event_Type,
-                             Wait_for_Hot_Water,
-                             Include_Behavior_Wait,
-                             Behavior_Wait_Trigger,
-                             Behavior_wait,
-                             Use_time,
-                             Flow_rate_waiting,
-                             Flow_rate_use,
-                             Hot_Water_Temp,
-                             Threshold_Temp,
-                             Ambient_Temp)]
+                         Day_num      = .GRP,
+                         Event_Index  = 0, # place holder only
+                         Start_Time,
+                         Fixture_ID,
+                         Event_Type,
+                         Wait_for_Hot_Water,
+                         Include_Behavior_Wait,
+                         Behavior_Wait_Trigger,
+                         Behavior_wait,
+                         Use_time,
+                         Flow_rate_waiting,
+                         Flow_rate_use,
+                         Hot_Water_Temp,
+                         Threshold_Temp,
+                         Ambient_Temp,
+                         Cold_Water_Temp), 
+                  by=c('DHWDAYUSE')][
+                    order(DHWDAYUSE, Start_Time) ]
 
-# make the cool down rows
-DT_cool_down_rows <- data.table( Event_Index = seq(2,2*nrow(DT_1day_schedule),by=2))
+# fix Event_Index
+DT_draw_schedule[ , Event_Index := 1:nrow(DT_draw_schedule)]
 
-# merge in the cool down rows
-DT_1day_schedule <- 
-merge(DT_1day_schedule, DT_cool_down_rows, by="Event_Index", all = TRUE)
+# check the Event_Index spans DHWDAYUSE Day_num 
+DT_draw_schedule[ , list(DHWDAYUSE      = unique(DHWDAYUSE),
+                         lo.Event_Index = min(Event_Index),
+                         hi.Event_Index = max(Event_Index)),
+                  by=Day_num]
 
-# set Event_Type for cool down rows
-DT_1day_schedule[is.na(Event_Type), Event_Type := 'No use, cool down']
+str(DT_draw_schedule)
 
 # save to csv file
-fwrite(DT_1day_schedule, file=paste0(wd_data,"1day_schedule.csv"))
+fwrite(DT_draw_schedule, file=paste0(wd_data,"day_schedule.csv"),
+       quote = TRUE)
