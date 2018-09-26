ded950047d6e19481065b0f77a808fd3b96aa2cf
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-09-26 15:31:50 -0700

    horizontal bar charts for energy

--- results_charts.R
+++ results_charts.R
@@ -14,3 +14,35 @@ source("setup_wd.R")
 load(file = paste0(wd_data,"DT_results.Rdata"))
 
 names(DT_results)
+
+# convert distributed core, normal flow to long data
+DT_results_long <-
+  melt(DT_results[core == 'distributed' & 
+                    flow == 'std' & 
+                    !is.na(identifier)],
+       id.vars = c('identifier'),
+       measure.vars = c('HW_energy_excess',
+                        'HW_energy_not_met')
+    )
+
+# chart distributed core, normal flow
+ggplot(data = DT_results_long) +
+  # colums of HW_energy_excess 
+  geom_col(aes(x=identifier, y=value, fill=variable),
+           position = "dodge", width = .5) +
+  # rotate axis tick labels
+  theme(axis.text.x = element_text(angle = 90, 
+                                   hjust = 1,
+                                   size = 10)) +
+  coord_flip()
+
+
+
+  # colums of HW_energy_excess 
+  geom_col(aes(x=identifier, y=HW_energy_not_met),
+           width =.1, position = "dodge", color='red') 
+  
+  
+  DT_results[core == 'distributed' & flow == 'std' & !is.na(identifier),
+           list(case, layout, identifier, 
+                HW_energy_excess, HW_volume_excess)][order(case)]
