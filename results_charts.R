7aa97c85f9e24002cd1b15cd66f984474ba45f3d
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-09-26 23:05:38 -0700

    compact core, wasted energy, current practice pipe size, standard flow.

--- results_charts.R
+++ results_charts.R
@@ -1,7 +1,7 @@
 # results_charts.R
-# script to plot energy wasted, water wasted, and loads not met
-# for compact and distributed core
-# and for normal and low flow
+# script to plot energy wasted and loads not met
+# for distributed core
+# for normal and low flow
 # Mon Sep 24 14:40:25 2018
 
 # set packages & etc
@@ -15,29 +15,51 @@ load(file = paste0(wd_data,"DT_results.Rdata"))
 
 names(DT_results)
 
-# get order of identifier
-# DT_identifier_index <- 
-#   DT_results[identifier!='', list(index, identifier)]
-# setkey(DT_identifier_index,index)
+# change the variable names
+setnames(DT_results,
+         old = c('HW_energy_excess','HW_energy_not_met','HW_volume_excess'),
+         new = c('Wasted Energy','Loads Not Met','Wasted Water')
+         )
+
+
+# find max values
+DT_results[(core == 'distributed' & identifier!='') |
+            core == 'compact' ,
+           list(max_Wasted_Energy = max(`Wasted Energy`),
+                max_Loads_Not_Met = max(`Loads Not Met`),
+                max_Wasted_Water = max(`Wasted Water`)
+                ),
+           by = c('flow','core')
+                ]
+#    flow        core max_Wasted_Energy max_Loads_Not_Met max_Wasted_Water
+# 1:  low     compact          2427.880          2939.532         5.312066
+# 2:  std     compact          2366.549          3453.936         5.177879
+# 3:  low distributed          2885.546          3129.780         6.313415
+# 4:  std distributed          2554.302          3943.074         5.588671
+
 
 # ordered list of identifiers
 l_identifier = DT_results[index %in% 106:125]$identifier
 
+# --- distributed core, normal flow ---
 # convert distributed core, normal flow to long data
 DT_results_long <-
   melt(DT_results[core == 'distributed' & 
                     flow == 'std' & 
                     identifier!=''],
        id.vars = c('identifier', 'index'),
-       measure.vars = c('HW_energy_excess',
-                        'HW_energy_not_met')
+       measure.vars = c('Wasted Energy',
+                        'Loads Not Met')
     )
 
-# change the variable names
-DT_results_long[ variable=="HW_energy_excess", variable := "Wasted Energy" ]
-DT_results_long[ variable=="HW_energy_not_met", variable := "Loads Not Met" ]
-
 str(DT_results_long)
+
+# change 'variable' from factor to character
+# DT_results_long[ , variable := as.character(variable)]
+
+# set the color choices
+colorchoices <- c("Wasted Energy" = "pink", 
+                  "Loads Not Met" = "light blue")
 
 # chart distributed core, normal flow
 ggplot(data = DT_results_long) +
@@ -45,11 +67,13 @@ ggplot(data = DT_results_long) +
   geom_col(aes(x=identifier, y=value, fill=variable),
            position = "dodge", width = .5) +
   
-  # set the colors
-  scale_color_manual(values = c("Attending_Education" = "dark green", "Not_AE" = "red"))
+  scale_color_manual(values = colorchoices,
+                     aesthetics = c("fill")) +
 
+  # fix the y scale
+  scale_y_continuous(limits = c(0,4000)) +
   
-  # set expand the x-axis for labels
+  # set for expand the x-axis for labels to fit in group labels
   scale_x_discrete( expand = expand_scale(mult = -0.1, add = 3),
                     # reverse order for when plot flipped
                     limits = rev(l_identifier)) +
@@ -61,17 +85,21 @@ ggplot(data = DT_results_long) +
   theme(axis.text.y = element_text(vjust = 1,
                                    hjust = 0,
                                    #angle = 90,
-                                   size = 10))   +
+                                   size = 5))   +
   
   # add the labels
   labs(title = "Wasted Energy and Loads Not Met",
+       subtitle = "distributed core, normal flow",
        y = "energy (BTU)") +
   
   # center the title
-  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5, size = 5)) +
   
   # clean up the legend
-  guides(fill = guide_legend(title = NULL) ) +
+  guides(fill = guide_legend(title = NULL,
+                             reverse = TRUE)
+         ) +
   
   # add some text
   # x values for data range from 1 to 20, for the numbered categories
@@ -82,17 +110,98 @@ ggplot(data = DT_results_long) +
                      "Mini-Manifold",
                      "Central Manifold",
                      "Two Water Heaters",
-                     "One Trunk ")
+                     "One Trunk "),
+           size = 2
+  ) +
+  
+  # adjust text size for png plot
+  theme(legend.text = element_text(size = 5)) 
+  
+  
+# save chart
+ggsave(filename = paste0("distributed_normal_wasted_energy.png"), path=wd_charts,
+       width = 5.25, height = 4 )
+
+
+# cut and pasted same graph for low flow
+# --- distributed core, low flow ---
+# convert distributed core, low flow to long data
+DT_results_long <-
+  melt(DT_results[core == 'distributed' & 
+                    flow == 'low' & 
+                    identifier!=''],
+       id.vars = c('identifier', 'index'),
+       measure.vars = c('Wasted Energy',
+                        'Loads Not Met')
   )
-  
 
+str(DT_results_long)
 
+# change 'variable' from factor to character
+# DT_results_long[ , variable := as.character(variable)]
 
+# set the color choices
+colorchoices <- c("Wasted Energy" = "pink", 
+                  "Loads Not Met" = "light blue")
+
+# chart distributed core, normal flow
+ggplot(data = DT_results_long) +
   # colums of HW_energy_excess 
-  geom_col(aes(x=identifier, y=HW_energy_not_met),
-           width =.1, position = "dodge", color='red') 
+  geom_col(aes(x=identifier, y=value, fill=variable),
+           position = "dodge", width = .5) +
   
+  scale_color_manual(values = colorchoices,
+                     aesthetics = c("fill")) +
   
-  DT_results[core == 'distributed' & flow == 'std' & !is.na(identifier),
-           list(case, layout, identifier, 
-                HW_energy_excess, HW_volume_excess)][order(case)]
+  # fix the y scale
+  scale_y_continuous(limits = c(0,4000)) +
+  
+  # set for expand the x-axis for labels to fit in group labels
+  scale_x_discrete( expand = expand_scale(mult = -0.1, add = 3),
+                    # reverse order for when plot flipped
+                    limits = rev(l_identifier)) +
+  
+  # turn plot on it's side
+  coord_flip() +
+  
+  # fuss with category axis tick labels
+  theme(axis.text.y = element_text(vjust = 1,
+                                   hjust = 0,
+                                   #angle = 90,
+                                   size = 5))   +
+  
+  # add the labels
+  labs(title = "Wasted Energy and Loads Not Met",
+       subtitle = "distributed core, low flow",
+       y = "energy (BTU)") +
+  
+  # center the title
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5, size = 5)) +
+  
+  # clean up the legend
+  guides(fill = guide_legend(title = NULL,
+                             reverse = TRUE)
+  ) +
+  
+  # add some text
+  # x values for data range from 1 to 20, for the numbered categories
+  annotate("text", 
+           x = c(20.5, 15.5, 10.5,  5.5, 3.5), 
+           y = 1000, 
+           label = c("Trunk and Branch",
+                     "Mini-Manifold",
+                     "Central Manifold",
+                     "Two Water Heaters",
+                     "One Trunk "),
+           size = 2
+  ) +
+  
+  # adjust text size for png plot
+  theme(legend.text = element_text(size = 5)) 
+
+
+# save chart
+ggsave(filename = paste0("distributed_low_flow_wasted_energy.png"), path=wd_charts,
+       width = 5.25, height = 4 )
+
---
+++ results_charts_compact_energy.R
@@ -0,0 +1,167 @@
+# results_charts_compact_energy.R
+# script to plot energy wasted and loads not met
+# for compact core
+# for normal and low flow
+# Mon Sep 24 14:40:25 2018
+
+# set packages & etc
+source("setup.R")
+
+# set up paths to working directories
+source("setup_wd.R")
+
+# load the results data
+load(file = paste0(wd_data,"DT_results.Rdata"))
+
+names(DT_results)
+
+# change the variable names
+setnames(DT_results,
+         old = c('HW_energy_excess','HW_energy_not_met','HW_volume_excess'),
+         new = c('Wasted Energy','Loads Not Met','Wasted Water')
+         )
+
+
+# find max values
+DT_results[(core == 'distributed' & identifier!='') |
+            core == 'compact' ,
+           list(max_Wasted_Energy = max(`Wasted Energy`),
+                max_Loads_Not_Met = max(`Loads Not Met`),
+                max_Wasted_Water = max(`Wasted Water`)
+                ),
+           by = c('flow','core')
+                ]
+#    flow        core max_Wasted_Energy max_Loads_Not_Met max_Wasted_Water
+# 1:  low     compact          2427.880          2939.532         5.312066
+# 2:  std     compact          2366.549          3453.936         5.177879
+# 3:  low distributed          2885.546          3129.780         6.313415
+# 4:  std distributed          2554.302          3943.074         5.588671
+
+# change from wet room rectangle to laundry
+DT_results[core == 'compact', list(n=.N), by=WH_location ]
+DT_results[core == 'compact' & 
+             WH_location == 'WH in wet room rectangle', 
+           WH_location := 'laundry']
+DT_results[core == 'compact' & 
+             WH_location == 'WH in garage', 
+           WH_location := 'garage']
+str(DT_results)
+
+names(DT_results)
+
+# figure out what to plot
+DT_results[core == 'compact' , list(case, layout, flow, WH_location)]
+DT_results[core == 'compact' , list(case, layout, flow, WH_location, identifier)]
+
+# different charts for std|low
+# split layout into layout and pipe_size
+# different facets for pipe_size
+#   need column for pipe_size 'current practice', '3/8 pipe', 'no 1 inch' '3/8 pipe & no 1 inch'
+# melt id.vars flow, pipe_size, layout, WH_location
+#   measure.vars c('Wasted Energy', 'Loads Not Met'))
+
+# split layout into layout and pipe_size
+DT_results[core == 'compact', list(n=.N),by=layout]
+
+# layout2
+DT_results[core == 'compact' & str_detect(layout, 'Trunk&Branch'), 
+           layout2 := 'Trunk&Branch']
+DT_results[core == 'compact' & str_detect(layout, 'Mini-manifold'), 
+           layout2 := 'Mini-manifold']
+DT_results[core == 'compact' & str_detect(layout, 'Home Run'), 
+           layout2 := 'Home Run']
+DT_results[core == 'compact' & str_detect(layout, 'One-zone'), 
+           layout2 := 'One-zone']
+DT_results[core == 'compact', list(n=.N),by=layout2]
+
+# pipe_size
+DT_results[core == 'compact' & !str_detect(layout, 'pipe'), 
+           pipe_size := 'current practice']
+DT_results[core == 'compact' & str_detect(layout, "& 3/8 pipe$"), 
+           pipe_size := '3/8 pipe']
+DT_results[core == 'compact' & str_detect(layout, '& 3/8 pipe & no 1 inch pipe'), 
+           pipe_size := '3/8 pipe & no 1 inch pipe']
+DT_results[core == 'compact' & is.na(pipe_size), 
+           pipe_size := 'no 1 inch pipe']
+DT_results[core == 'compact', list(n=.N),by=pipe_size]
+
+# clean up layout
+DT_results[ , layout := NULL]
+setnames(DT_results, old = c('layout2'), new = c('layout'))
+
+# check layout and pipe_size
+DT_results[core == 'compact', list(n=.N),by=c('layout','pipe_size')]
+
+# add a pipe_size_factor in the desired order
+DT_results[core == 'compact', 
+           pipe_size_factor := factor(pipe_size, 
+                                      levels=c('current practice',
+                                               '3/8 pipe',
+                                               'no 1 inch pipe',
+                                               '3/8 pipe & no 1 inch pipe')
+                                      )
+           ]
+
+# --- compact core, normal flow ---
+# convert compact core, normal flow to long data
+DT_results_long <-
+  melt(DT_results[core == 'compact'],
+       id.vars = c('flow', 'pipe_size_factor', 'layout', 'WH_location'),
+       measure.vars = c('Wasted Energy',
+                        'Loads Not Met')
+    )
+
+str(DT_results_long)
+
+# change 'variable' from factor to character
+# DT_results_long[ , variable := as.character(variable)]
+
+# set the color choices
+colorchoices <- c("Wasted Energy" = "pink", 
+                  "Loads Not Met" = "light blue")
+
+# for testing
+test_flow = 'std'
+test_pipe_size = 'current practice'
+test_layout = NULL
+
+
+# chart distributed core, flow
+ggplot(data = DT_results_long[flow==test_flow],
+       aes(label=WH_location)) +
+  # colums of HW_energy_excess 
+  geom_col(aes(x=WH_location, y=value, fill=variable),
+           position = "dodge", width = .5) +
+  
+  scale_color_manual(values = colorchoices,
+                     aesthetics = c("fill")) +
+  
+  # fix the y scale
+  scale_y_continuous(limits = c(0,4000)) +
+  
+  # facet
+  facet_wrap(vars(layout)) +
+  # facet_wrap(vars(pipe_size_factor))
+  
+  # add the labels
+  labs(title = "Wasted Energy and Loads Not Met",
+       subtitle = "compact core, normal flow",
+       y = "energy (BTU)",
+       x = "water heater location") +
+  
+  # center the title
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) +
+  
+  # clean up the legend
+  guides(fill = guide_legend(title = NULL)
+         ) +
+
+    # adjust text size for png plot
+  theme(legend.text = element_text(size = 5)) 
+  
+  
+# save chart
+ggsave(filename = paste0("compact_normal_wasted_energy.png"), path=wd_charts,
+       width = 5.25, height = 4 )
+
