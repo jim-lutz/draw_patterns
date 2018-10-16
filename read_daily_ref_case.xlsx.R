29353108c6be62c9acd6854c3fd58cee50a4c2f4
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-10-15 21:53:20 -0700

    output of daily summary tables as csv files.

--- read_daily_ref_case.xlsx.R
+++ read_daily_ref_case.xlsx.R
@@ -92,12 +92,12 @@ DT_ref_results[,`:=`(
   ]
 
 # multiply % by 100
-DT_ref_results[,`:=`(
-  `Energy Efficiency %` = `Energy Efficiency %` * 100,
-  `Water Efficiency %`  = `Water Efficiency %`  * 100,
-  `Time Efficiency %`   = `Time Efficiency %`   * 100,
-  `Load not met (%)`    = `Load not met (%)`    * 100)
-  ]
+# DT_ref_results[,`:=`(
+#   `Energy Efficiency %` = `Energy Efficiency %` * 100,
+#   `Water Efficiency %`  = `Water Efficiency %`  * 100,
+#   `Time Efficiency %`   = `Time Efficiency %`   * 100,
+#   `Load not met (%)`    = `Load not met (%)`    * 100)
+#   ]
   
 # remove Recirc & Cooldown
 DT_ref_results <-
@@ -108,3 +108,58 @@ DT_ref_results <-
 DT_ref_results[ is.nan(`Load not met (%)`),
                 `Load not met (%)` := NA]
 
+# order as in Final Report
+DT_order <- data.table( 
+  `Fixture ID` = c(
+    'MB_SH', 'MB_SK1', 'MB_SK2', 'MB_TB', 'K_SK', 'K_DW', 'LN_WA',
+    'B2_SH', 'B2_SK1', 'B2_SK2', 'B2_TB', 'B3_SK', 'Total'),
+  ord = (1:13)
+  )
+
+# merge onto DT_ref_results
+DT_ref_results <- merge(DT_ref_results, DT_order, by='Fixture ID' )
+
+# sort by ord
+setkey(DT_ref_results, ord)
+DT_ref_results[, list(`Fixture ID`, ord)]
+
+# output to csv
+# the energy table
+write_excel_csv(DT_ref_results[,list(
+            `Fixture ID`, 
+            `HW Supply Energy (Btu)`, `To fixture - used`, `To fixture - wasted`,
+            `Lost to Ambient - during use`, `Stored in Pipe and Insulation`,
+            `Energy Efficiency %`)],
+          path = paste0(wd_data, "ref_day_energy_table.csv"),
+          na = "")
+
+# the water table
+write_excel_csv(DT_ref_results[,list(
+            `Fixture ID`, 
+            `Water Volume Supplied (Gallon)`,
+            `Water Volume Used (Gallon)`,
+            `Water Volume Wasted (Gallon)`,
+            `Water Efficiency %`)],
+          path = paste0(wd_data, "ref_day_water_table.csv"),
+          na = "")
+
+# the time table
+write_excel_csv(DT_ref_results[,list(
+            `Fixture ID`, 
+            `Structural Waiting (Sec)`,
+            `Behavior Waiting (Sec)`,
+            `Total Waiting (Sec)`,
+            `Use Duration (Sec)`,
+            `Time Efficiency %`)],
+          path = paste0(wd_data, "ref_day_time_table.csv"),
+          na = "")
+
+# the service table
+write_excel_csv(DT_ref_results[,list(
+            `Fixture ID`, 
+            `Theoretical HW demand (Btu)`,
+            `HW Energy To fixture - used`,
+            `Load not met (Btu)`,
+            `Load not met (%)`)],
+          path = paste0(wd_data, "ref_day_service_table.csv"),
+          na = "")
