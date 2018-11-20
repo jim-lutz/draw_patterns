38a19dedd0235a75b87d6198e4a343b50c2f0d71
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-11-20 13:00:39 -0800

    charts for tables 19 & 24 as a macro to source

---
+++ chart_19_24.R
@@ -0,0 +1,101 @@
+# chart_19_24.R
+# a macro to make charts for table 19 & 24 from draft final report v.10
+# Distributed Wet Room Layouts, Normal Pipe, Normal | Low Flow
+# uses
+#   DT_relative_long_chart    the data
+#   tn                        the table number
+
+# set the color choices, using grey and black for photocopying
+colorchoices <- c("Energy Wasted (%)" = "gray74", 
+                  "Load not Met (%)" = "black")
+
+# find the png file name parameters
+DT_chart_params <-
+  DT_relative_long[table==tn,
+                   list(core      = unique(core),
+                        flow      = unique(flow),
+                        smallpipe = unique(smallpipe))]
+
+# build the png filename for the chart
+fn <-
+  with(DT_chart_params,
+       {
+         pipe<-ifelse(smallpipe,"norm","small")
+         paste0(core,"_core_",pipe,"_pipe_",flow,"_flow")
+       }
+  )
+
+# get the data
+DT_relative_long_chart <-
+  DT_relative_long[ table == tn, ]
+
+DT_relative_long_chart[, list(Identification, Configuration, table)]
+# don't forget both energy wasted and loads not met are in here
+
+str(DT_relative_long_chart)
+
+# get the order for Identification
+Identification.order <- DT_relative_long_chart[variable=="Load not Met (%)"]$Identification
+
+# add gaps before each group of Configuration
+Identification.order <- 
+  c("", Identification.order[1:5],    # Trunk & 3 Branches
+    "", Identification.order[6:10],   # Hybrid Mini-Manifold
+    "", Identification.order[11:15],  # Central Manifold
+    "", Identification.order[16:17],  # Two Heaters
+    "", Identification.order[18:20])  # One-zone w/o Recirc
+
+# reverse it so it will show up in right order in the charts
+Identification.order <- rev(Identification.order)
+
+# chart using this data 
+ggplot(data = DT_relative_long_chart) +
+  
+  # colums of 'Energy Wasted (%)' and 'Load not Met (%)'
+  geom_col(aes(x=Identification, y=value, fill=variable),
+           position = "dodge", width = .5) +
+  
+  # get specify the right order
+  scale_x_discrete(limits = Identification.order) +
+  
+  # turn plot on it's side
+  coord_flip() +
+  
+  # remove the Identification label
+  xlab("") +
+  
+  # label the percentages
+  scale_y_continuous(name = 'Percent', 
+                     labels = c('0%','10%','20%','30%','40%','50%','60%'), 
+                     breaks=c(0,0.1,0.2,0.3,0.4,0.5,0.6),
+                     limits = c(0, 0.6)) +
+  
+  # clean up the legend
+  guides(fill = guide_legend(title = NULL, reverse = TRUE)) +
+  
+  # add some text
+  annotate("text", 
+           x = c(25, 19, 13,  7, 4),  # find from Identification.order 
+           y = .1, hjust = 0,
+           label = unique(DT_relative_long_chart$Configuration),
+           size = 4
+  ) + #
+  
+  # specify color of bars
+  scale_colour_manual(values = c("gray74", "black"),
+                      aesthetics = c("colour", "fill")) +
+  
+  # specify location of legend
+  theme(legend.position = "bottom", # c(0.9, .95),
+        legend.background = element_rect( size=0.25, 
+                                          linetype="solid",
+                                          color = "black")
+  )
+
+# get date to include in file name
+d <- format(Sys.time(), "%F")
+
+# save chart
+ggsave(filename = paste0("relative_",fn,"_",d,".png"), 
+       path=wd_charts) # , width = 5.25, height = 4 
+# Saving 6.21 x 4.42 in image
--- summary_charts.R
+++ summary_charts.R
@@ -52,6 +52,18 @@ DT_relative <-
                 str_detect(Configuration, "w/ ") ) ,
               ]
 
+# find max values
+DT_relative[,
+            list(max_Wasted_Energy = max(`Energy Wasted (%)`),
+                 max_Wasted_Water  = max(`Water Wasted (%)`),
+                 max_Wasted_Time   = max(`Time Wasted (%)`),
+                 max_Loads_Not_Met = max(`Load not Met (%)`)
+            )]
+#    max_Wasted_Energy max_Wasted_Water max_Wasted_Time max_Loads_Not_Met
+# 1:         0.5608854        0.5608854       0.1820058         0.2527405
+# set max to 60% across all charts
+
+
 # list of all the Configuration
 DT_relative[ , list(n=length(Identification)), by=Configuration]
 
@@ -115,10 +127,13 @@ DT_relative[,list(n=length(unique(Configuration))), by = table][order(table)]
 DT_relative[table %in% c("20",'21','22',"25",'26','27'),
             list(Identification=unique(Identification)), by = table][order(table)]
 
+# save as csv
+write_excel_csv(DT_relative,
+                path = paste0(wd_data, "data_relative_charts.csv"),
+                na = "")
 
 
-
-# convert to long data, so can group by load not met or energy wasted
+# convert to long data, so can group by loads not met and energy wasted
 DT_relative_long <-
   melt(DT_relative[],
        id.vars = c('Configuration', 'Identification', 'PipeSize', 'table',
@@ -129,97 +144,16 @@ DT_relative_long <-
 names(DT_relative_long)
 str(DT_relative_long)
 
+# start making the charts
 
-# set the color choices, using grey and black for photocopying
-colorchoices <- c("Energy Wasted (%)" = "gray74", 
-                  "Load not Met (%)" = "black")
+# table number
+tn <- '19'
 
+# make the chart for table 19
+source("chart_19_24.R")
 
-# chart for table 19
-# Distributed Wet Room Layouts, Normal Pipe, Normal Flow
+# table number
+tn <- '24'
 
-# make a file name
-fn <- "Distributed_Wet_Room_Layouts_Normal_Pipe_Normal_Flow"
-
-# find the data
-DT_relative_long_19 <-
-DT_relative_long[ table == '19', ]
-
-DT_relative_long_19[, list(Identification, Configuration, table)]
-# don't forget both energy wasted and loads not met are in here
-
-str(DT_relative_long_19)
-
-# get the order for Identification
-Identification.order <- DT_relative_long_19[variable=="Load not Met (%)"]$Identification
-
-# add gaps before each group of Configuration
-Identification.order <- 
-  c("", Identification.order[1:5],    # Trunk & 3 Branches
-    "", Identification.order[6:10],   # Hybrid Mini-Manifold
-    "", Identification.order[11:15],  # Central Manifold
-    "", Identification.order[16:17],  # Two Heaters
-    "", Identification.order[18:20])  # One-zone w/o Recirc
-
-# reverse it so it show up in right order in chart
-Identification.order <- rev(Identification.order)
-  
-# save as csv
-write_excel_csv(DT_relative_long_19,
-                path = paste0(wd_data, fn, ".csv"),
-                na = "")
-
-# chart using this data 
-ggplot(data = DT_relative_long_19) +
-  
-  # colums of 'Energy Wasted (%)' and 'Load not Met (%)'
-  geom_col(aes(x=Identification, y=value, fill=variable),
-         position = "dodge", width = .5) +
-  
-  # get specify the right order
-  scale_x_discrete(limits = Identification.order) +
-  
-  # turn plot on it's side
-  coord_flip() +
-
-  # remove the Identification label
-  xlab("") +
-  
- # label the percentages
-  scale_y_continuous(name = 'Percent', 
-                     labels = c('0%','10%','20%','30%','40%','50%'), limits = NULL,
-                     expand = waiver(), na.value = NA_real_,
-                     trans = "identity", position = "left", sec.axis = waiver()) +
-
-  # clean up the legend
-  guides(fill = guide_legend(title = NULL, reverse = TRUE)) +
-  
-  # add some text
-  annotate("text", 
-           x = c(25, 19, 13,  7, 4),  # find from Identification.order 
-           y = .1, hjust = 0,
-           label = unique(DT_relative_long_19$Configuration),
-           size = 4
-           ) + #
-  
-  # specify color of bars
-  scale_colour_manual(values = c("gray74", "black"),
-                      aesthetics = c("colour", "fill")) +
-  
-  # specify location of legend
-  theme(legend.position = "bottom", # c(0.9, .95),
-        legend.background = element_rect( size=0.25, 
-                                          linetype="solid",
-                                          color = "black")
-        )
-  
-  #   # adjust text size for png plot
-  # theme(legend.text = element_text(size = 5)) 
-
-# get date to include in file name
-d <- format(Sys.time(), "%F")
-
-# save chart
-ggsave(filename = paste0("relative_",fn,"_",d,".png"), 
-       path=wd_charts) # , width = 5.25, height = 4 
-# Saving 6.21 x 4.42 in image
+# make the chart for table 24
+source("chart_19_24.R")
